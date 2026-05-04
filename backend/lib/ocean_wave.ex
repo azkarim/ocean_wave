003ae9b4b7638564f0d7593f904733c5f4683393
Azizul Karim <karimazizul1@gmail.com>
2026-05-05 02:52:27 +0600

    structure app to integrate a backend

---
+++ backend/lib/ocean_wave.ex
@@ -0,0 +1,18 @@
+defmodule OceanWave do
+  @moduledoc """
+  Documentation for `OceanWave`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OceanWave.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ backend/test/ocean_wave_test.exs
@@ -0,0 +1,8 @@
+defmodule OceanWaveTest do
+  use ExUnit.Case
+  doctest OceanWave
+
+  test "greets the world" do
+    assert OceanWave.hello() == :world
+  end
+end
